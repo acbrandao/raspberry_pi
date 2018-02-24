818d858411feb732a2c0f533d4cdb0e8177db858
Tony Brandao <abrandao29@gmail.com>
2018-02-24 00:16:01 -0500

    lots of enhancements and cod eupdates, fixed various mqtt issues, improved messages

--- subled.py
+++ subled.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import math
 import time, datetime
 import signal
@@ -17,13 +18,14 @@ import json
 import socket
 import re
 import urllib2 
+import sqlite3 #for logging and storing the data
 
 # MQTT Specific
 mqtt_broker_ip = "localhost"		# Local MQTT broker
 mqtt_port = 1883			# Local MQTT port
 mqtt_user = ""			# Local MQTT user
 mqtt_pass = ""			# Local MQTT password
-mqtt_topic = "sensor/door"		# Local MQTT topic to monitor
+
 localTimeOut = 120			# Local MQTT session timeout
 mqtt_timer = int(round(time.time() ))  #times mqtt between reuqsts
 
@@ -38,7 +40,10 @@ html_last_url=None
 
 #Quiet Hours time periods in which alerts will not show
 quiet_start=datetime.time(10,30,00)
-quiet_end =datetime.time(06,30,00)
+quiet_end =datetime.time(00,8,00)
+
+#database
+db=None
 
 def in_between(now, start, end):
     if start <= end:
@@ -74,6 +79,7 @@ def scrape_url(url, start_tag,end_tag):
 
 
 def getWeather():
+	#scrape site to get weather
 	temp = scrape_url("http://forecast.weather.gov/MapClick.php?lat=40.732&lon=-74.1742#.Won5xainE2w",'<p class="myforecast-current-lrg">','</p>')
 	temp=temp.replace('&deg;',' ')
 	print 'Newark Weather: [ ' + temp + " ]"	
@@ -83,12 +89,59 @@ def getWeather():
 
 
 def getExchangeRate():
-
+	#scrape site to get exchange rate
 	usdeur = scrape_url("https://www.bloomberg.com/quote/USDEUR:CUR",'<div class="price">','</div>')
 	print 'EURO->USD: [ ' + usdeur + " ]"	
 	scroll(" 1 USD= "+ ("%0.2f" % float(usdeur) ) + " EUR ")
 	return None
 
+##############################################
+#database functions for logging events and data
+###########################################
+def setup_db(dbname='log.db'):
+	db=None
+	if os.path.isfile(dbname)==True: #file exists open it
+		db = sqlite3.connect(dbname)
+		#open the database
+	else:
+		# Get a cursor object
+		db = sqlite3.connect(dbname)
+		cursor = db.cursor()
+		cursor.execute('''
+		CREATE TABLE [event] (
+			[id] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
+			[timestamp] VARCHAR(64)  NULL,
+			[topic] VARCHAR(64)  NULL,
+			[payload] VARCHAR(128)  NULL,
+			[source] VARCHAR(32)  NULL
+			)
+		''')
+		db.commit()
+		#create a new database
+	return db
+	
+def writedb_log(topic,payload,source):
+	try:
+		# Open the database
+		db = setup_db()  #create or OPEN Existing database
+		cursor = db.cursor()
+
+		# Insert user 1
+		cursor.execute('''INSERT INTO event (timestamp,topic,payload,source)
+					  VALUES(?,?,?,?)''', ('CURRENT_TIMESTAMP',topic,payload,source))
+		print('First user inserted')
+		id = cursor.lastrowid
+		print('Last row id: %d' % id)
+
+		db.commit()
+	# Catch the exception
+	except Exception as e:
+		  print 'Database run-time error:', e
+		
+	finally:
+		# Close the db connection
+		db.close()
+	return
 
 ##############################################
 #timer class to handle timer events in a perate thread
@@ -108,6 +161,10 @@ def hournow():
 	scroll( time.strftime("%H:%M:%S") )
 	return None
 	
+##############################################
+# ScrollPhat HD effects and Message routines
+###########################################
+
 def clocktick():
 # Display the hour as two digits
 	scrollphathd.clear()  # so we can rebuild it
@@ -120,6 +177,11 @@ def clocktick():
 
 def scroll(textmsg="???",bright=0.3,speed=0.04):
 	global scrollphathd
+	
+	#if quiet hours do not display
+	if is_quiet_time():
+		return None
+	
 	#  Clear buffer
 	# Reset the animation
 	scrollphathd.scroll_to(0, 0)
@@ -129,7 +191,7 @@ def scroll(textmsg="???",bright=0.3,speed=0.04):
 	# Keep track of the X and Y position for the rewind effect
 	pos_x = 0
 
-	textmsg=" "+textmsg+"         ."  # append 3 spaces tp message  to prevent wrapping of start
+	textmsg=" "+textmsg+"   "  # append 3 spaces tp message  to prevent wrapping of start
 	print "Scrolling  %s. size: %d" % (textmsg , len(textmsg  ) )
 	scrollphathd.write_string(textmsg, x=0, y=0, font=font5x7, brightness=bright)
 	
@@ -148,6 +210,11 @@ def scroll(textmsg="???",bright=0.3,speed=0.04):
 
 def show(textmsg="???",bright=0.5):
 	global scrollphathd
+	
+	#if quiet hours do not display
+	if is_quiet_time():
+		return None
+	
 	 #  Clear buffer
 	scrollphathd.clear()  # so we can rebuild it
 	scrollphathd.show()
@@ -155,6 +222,15 @@ def show(textmsg="???",bright=0.5):
 	scrollphathd.show()
 
 	return None
+	
+def swipe():
+		for x in range(18):
+			scrollphathd.fill(0.1,0,0,x,7)
+			scrollphathd.show()
+		for x in range(18):
+			scrollphathd.fill(0,0,0,x,7)
+			scrollphathd.show()	
+		return;
 
 def fade(textmsg="???"):
         global scrollphathd
@@ -166,15 +242,20 @@ def fade(textmsg="???"):
 
 	for x in range(5,0,-1):
 		brite=0.05*x
-        	scrollphathd.write_string(textmsg, x=0, y=0, font=font5x7,brightness=brite)
-        	scrollphathd.show()
-		time.sleep(0.01)
-        #scrollphathd.scroll()
-        #time.sleep(0.05)
+		scrollphathd.write_string(textmsg, x=0, y=0, font=font5x7,brightness=brite)
+		scrollphathd.show()
+		time.sleep(0.1)
+		#scrollphathd.scroll()
+		#time.sleep(0.05)
         return
+		
+		
+##############################################
+#Paho MQTT Event Handlers
+###########################################
 
 def on_connect(client, userdata, flags, rc):
-	global mqtt_topic,mqtt_timer
+	global mqtt_timer
 	now=int(round(time.time() ))
 	elapsed= (now-mqtt_timer)
 	print "onConnect with result code ..."+str(elapsed)+"s"
@@ -184,30 +265,46 @@ def on_connect(client, userdata, flags, rc):
 	  show("RDY",0.2)
 	else:
 	  show(str(rc),0.1 )
-	
+	  
+	# Now lets subscrbe to messages from the broker
+	# Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+	mqtt_topics = ["sensor/door/state", "sensor/door/runtime", "sensor/door/rssi", "sensor/door/bootcount"]
+	for mqtt_topic in mqtt_topics:
+		client.subscribe(mqtt_topic)
+		print("Subscribing to: "+mqtt_topic)   
+
 	mqtt_timer=int(round(time.time() ))
-	client.subscribe(mqtt_topic)
+	
+	
+	return None
+
 
 def on_message(client, obj, msg):
 	global message,mqtt_timer
 
 	print("on_message Rcvd: "+ msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+	
+	if msg.retain==1:
+		print("This is a retained message:")
+		
 	message = msg.payload
 
 	#lts handle the diffferent payloads
+	if msg.topic=="sensor/door/state":
+		if (message=="1"):
+			show("OPN",0.5)  # Open is =1 
+		else:
+			swipe()
 
-	if msg.topic=="sensor/door":
-			if (message=="0"):
-				fade("CLOSE")
-			else:
-				show("OPN",0.5)
+	elif msg.topic=="sensor/door/bootcount":
+		print(msg.topic+"  "+message)
 
-	elif msg.topic=="sensor/running_time":
-			scroll(message,0.5)
 	else:
 		print("No Topic/Message hanlder available")
-		fade("OK")
+		show("OK",0.2)
 
+	writedb_log(msg.topic,message,"source")
 	mqtt_timer=int(round(time.time() ))
 	return
 	
@@ -232,23 +329,42 @@ def get_ip_address(ifname):
         0x8915,  # SIOCGIFADDR
         struct.pack('256s', ifname[:15])
     )[20:24])     
+	
+##############################################
+#Utility functions
+###########################################
+#determing if hourse are quiet time
+def  is_quiet_time():
+	global quiet_start, quiet_end
+	
+	now=datetime.datetime.now()
+	this_time=now.time()
+	
+	# print("Now  :"+this_time.strftime("%H:%M:%S") )
+	# print("Start:"+quiet_start.strftime("%H:%M:%S") )
+	# print("End  :"+quiet_end.strftime("%H:%M:%S") )
+	
+	if in_between( this_time, quiet_start,quiet_end ):
+		print "<<<< SHhhh QUIET HOURS - DISPLAY TURNED OFF >>>"
+		return True
+	else:
+		return False
+
+
 
 ########################
 # Main
 ########################
  
 if "__main__" == __name__:
-
+	
 	now=datetime.datetime.now()
 	this_time=now.time()
 	
-	print("Now:"+this_time.strftime("%H:%M:%S") )
+	print("Now  :"+this_time.strftime("%H:%M:%S") )
 	print("Start:"+quiet_start.strftime("%H:%M:%S") )
-	print("End:"+quiet_end.strftime("%H:%M:%S") )
-	print("Quiet" if in_between( this_time, quiet_start,quiet_end ) else "day")
- 	
-
-
+	print("End  :"+quiet_end.strftime("%H:%M:%S") )
+	
 	print timenow()+"Starting MQTT Subscriber main... "
 	client = mqtt.Client("piZeroLCD")  #Client MQTT object
 	
@@ -263,35 +379,38 @@ if "__main__" == __name__:
 	# Once everything has been set up, we can (finally) connect to the broker
 	# 1883 is the listener port that the MQTT broker is using
 	
-	print "Connecting to MQTT broker at ", mqtt_broker_ip + "\nPort: " + str(mqtt_port)
 	ip_address= get_ip_address('wlan0')
 	print "Localhost IP adress:"+ip_address
 	# Once we have told the client to connect, let the client object run itself
 	# client.loop_forever()   this funciton is blocking
 	scroll(" IP:"+ip_address,0.2,0.02)
+	
 
+
+	print "Connecting to MQTT broker at ", mqtt_broker_ip + "\nPort: " + str(mqtt_port)
+	
    #connect(host, port=1883, keepalive=60, bind_address="")
 	client.connect(mqtt_broker_ip, mqtt_port, 0)
 
-	#subscribe
-	print "Subscribing to :"+mqtt_topic
-	client.subscribe(mqtt_topic)
 	
-	#start the MQTT lient loop
+#subscribe
 	client.loop_start()	
-     
+	
 	# see schedule for examples https://pypi.python.org/pypi/schedule
 	schedule.every().hour.do(hournow)
-	schedule.every(30).minutes.do(getWeather)
+	schedule.every(60).minutes.do(getWeather)
 	schedule.every().hour.do(getExchangeRate)
-
-	hournow()
 	
+	
+	try:
+		while True:
+			schedule.run_pending()
+			client.loop(.1)  #blocks for 100ms
 
-	while True:
-		schedule.run_pending()
-		client.loop(.1)  #blocks for 100ms
+	except KeyboardInterrupt:
+		print "User Ended SubLed"
+		scrollphathd.fill(0)
+		scrollphathd.show()
+		client.disconnect()
 
-	#client.disconnect()
-
-	quit()
+		quit()
